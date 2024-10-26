fa9b7526d7c19a3ac3ba7d88bcdb1e1819714f97
Kentaro <s2010346@edu.cc.uec.ac.jp>
2024-10-26 16:34:31 +0900

    resolve module problems

---
+++ demo/index.js
@@ -0,0 +1,5 @@
+// Import @tensorflow/tfjs-core
+import * as tf from '@tensorflow/tfjs-core';
+// Adds the WebGL backend to the global backend registry.
+import '@tensorflow/tfjs-backend-webgl';
+import '@tensorflow/tfjs-backend-webgpu';
